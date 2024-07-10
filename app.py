cceca625952bbc8469f9b73888d8bad581dcc2d5
7thmaniac <varanasidbzt@gmail.com>
2024-07-10 02:38:24 -0400

    chatbot is completed and adding to git

---
+++ app.py
@@ -0,0 +1,46 @@
+
+
+from openai import OpenAI
+import streamlit as st
+
+
+Client = OpenAI(api_key=st.secrets.OpenAIAPI.openai_api_key )
+
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [
+      { "role": "system", "content": "Think as a trip advisor" }
+  ]
+
+
+def communicate():
+  messages = st.session_state["messages"]
+  user_message = {"role":"user", "content":st.session_state["user_input"]}
+  messages.append(user_message)
+
+  response = Client.chat.completions.create(
+      model = "gpt-3.5-turbo-1106",
+      messages = messages
+  )
+
+  bot_message = response.choices[0].message
+  messages.append(bot_message)
+
+  st.session_state["user_input"] = ""
+
+
+#user interface
+st.title ("Trip adviser AI")
+st.write ("Utilizing the chatGPT API, this chatbot offers advanced conversational capabilities.")
+
+user_input = st.text_input("please enter a message here.", key = "user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):
+        if isinstance(message, dict):
+            speaker = "😎" if message["role"] == "user" else "🤖"
+            st.write (speaker + ": " + message["content"])
+        else:
+            st.write("🤖: " + message.content)
+
